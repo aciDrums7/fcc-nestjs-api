8f836b0d831a664dffdc5dfae23e4641c033b1c0
aciDrums7 <edoardo.carradori.96@gmail.com>
2023-10-02 22:13:02 +0000

    Auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
+
+    @Post('signup')
+    signup() {
+        return this.authService.signup();
+    }
+
+    @Post('login')
+    login() {
+        this.authService.login();
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+    signup() {
+        return { msg: `I'm signed up` };
+    }
+
+    login() {
+        const newLocal = `I'm logged in`;
