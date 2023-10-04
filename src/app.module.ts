dc58fd95ab03522b1f271034a4eb6d3581392bdc
aciDrums7 <edoardo.carradori.96@gmail.com>
2023-10-04 15:42:12 +0000

    Added nestjs-form-data dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { NestjsFormDataModule } from 'nestjs-form-data';
 
 @Module({
-    imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+    imports: [
+        AuthModule,
+        UserModule,
+        BookmarkModule,
+        PrismaModule,
+        NestjsFormDataModule,
+    ],
     controllers: [],
     providers: [],
 })
