f9421341363f604acab9d1f46a6562835d045442
aciDrums7 <edoardo.carradori.96@gmail.com>
2023-10-06 16:20:16 +0000

    Hashing user password with argon + Refactoring of schema.prisma + Signup logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { AuthDto } from 'src/dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {}
 
-    signup(dto: AuthDto) {
-        return dto;
+    async signup(dto: AuthDto) {
+        //1 generate the password hash
+        const hashedPassword = await argon.hash(dto.password);
+        //2 save the new user in the db
+        try {
+            const user = await this.prisma.user.create({
+                data: { email: dto.email, password: hashedPassword },
+                select: {
+                    id: false,
+                    firstName: true,
+                    lastName: true,
+                    email: true,
+                    password: false,
+                    createdAt: true,
+                    updatedAt: true,
+                },
+            });
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                //? Prisma has specific errors, P2022 -> "Unique constraint failed on the {constraint}"
+                //5 https://www.prisma.io/docs/reference/api-reference/error-reference
+                if (error.code == 'P2002') {
+                    throw new ForbiddenException('Credentials taken');
+                }
+            } else {
+                throw error;
+            }
+        }
+        //3 return the new user
     }
 
     login() {
